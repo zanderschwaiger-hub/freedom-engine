28fbf4596c9c3e3f54ee29cb442e449b5131609f
zanderschwaiger-hub <zanderschwaiger@gmail.com>
2026-05-31 15:06:58 -0700

    Deploy Freedom Engine marketing site - src/main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import App from './App.jsx'
+import ThankYou from './pages/ThankYou.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/thank-you" element={<ThankYou />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+)
